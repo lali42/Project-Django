1a943641112e536427ab268d07a4e5e082af6fa9
sumet <sumet.sa.59@ubu.ac.th>
2019-10-22 10:32:07 +0700

    อัพขึ้น github ล่าสุด 20%

--- LalitaProject/settings.py
+++ LalitaProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '79yw&6)lt_49yf2miiksa!4&1izwai0g^^12minrfu2vcc)tdb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'lali42.pythonanywhere.com']
+ALLOWED_HOSTS = [u'lali42.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
